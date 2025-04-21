e2df0918d993212fa2177368ac2658193e3835f4
Itaji de Carvalho <itaji.1999@gmail.com>
2025-04-21 15:53:16 -0300

    Cria metodo para expor arquivos salvos na pasta uploads

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,14 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
   app.enableCors({
     origin: 'http://localhost:5173',
     credentials: true,
   });
+
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    prefix: '/uploads/',
+  });
+
   app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(process.env.PORT ?? 3002);
 }
 bootstrap();
