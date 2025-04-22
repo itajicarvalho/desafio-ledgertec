fcc8d8cdd3e2654935a0b4afe3f7a1118183d50a
Itaji de Carvalho <itaji.1999@gmail.com>
2025-04-22 14:08:29 -0300

    Adiciona campo dotenv do front no enviroment do back

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONT_URL,
     credentials: true,
   });
 
