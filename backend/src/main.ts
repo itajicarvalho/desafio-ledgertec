1cc63323a2d40a4f1308ca2b9dd532b909c5de22
Itaji de Carvalho <itaji.1999@gmail.com>
2025-04-22 10:42:16 -0300

    Cria arquivo de exemplo .env para o backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -18,6 +18,6 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(process.env.PORT ?? 3002);
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
